0740d934661b3d2c13ab9abc0e2373e8f9cce2a3
calc <calc@list.ru>
2014-04-16 13:05:33 +0400

    media element load only in view временно отдаем прямую ссылку, вместо переадресации на stream

--- protected/views/archive/view.php
+++ protected/views/archive/view.php
@@ -14,6 +14,13 @@ $this->menu=array(
 	array('label'=>'Delete Archive', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
 	array('label'=>'Manage Archive', 'url'=>array('admin')),
 );
+
+Yii::app()->clientScript->registerCSSFile('/mediaelement/build/mediaelementplayer.css');
+Yii::app()->clientScript->registerScriptFile('/mediaelement/build/jquery.js', CClientScript::POS_HEAD);
+Yii::app()->clientScript->registerScriptFile('/mediaelement/build/mediaelement-and-player.min.js', CClientScript::POS_HEAD);
+
+//Yii::app()->clientScript->registerCSSFile('http://vjs.zencdn.net/4.5/video-js.css');
+//Yii::app()->clientScript->registerScriptFile('http://vjs.zencdn.net/4.5/video.js', CClientScript::POS_HEAD);
 ?>
 
 <h1>View Archive #<?php echo $model->id; ?></h1>
@@ -26,7 +33,8 @@ $frame = new VideoFrame();
 //echo $frame->view($model->file);
 //http://10.154.28.208/rec/1/2014-03-16/17_22_47_UID_1__CID_9_rec.avi
 if($model->pathMp4()){
-    echo $frame->view($this->createUrl('stream',array('id'=>$model->id)));
+    //echo $frame->view($this->createUrl('stream',array('id'=>$model->id)));
+    echo $frame->view($this->getUrl($model->id));
 }
 else{
     if(!$model->pathAvi()){
--- themes/lk/views/layouts/main.php
+++ themes/lk/views/layouts/main.php
@@ -11,10 +11,6 @@
 
     <title><?php echo CHtml::encode($this->pageTitle); ?></title>
 
-    <?php //<!-- for video tag --> ?>
-    <script src="<?php echo Yii::app()->baseUrl; ?>/mediaelement/build/mediaelement-and-player.min.js"></script>
-    <link rel="stylesheet" href="<?php echo Yii::app()->baseUrl; ?>/mediaelement/build/mediaelementplayer.css" />
-
 </head>
 
 <body>
