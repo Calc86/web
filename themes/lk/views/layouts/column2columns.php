b472bf9fbb2c73e562b172fb669d239e49964930
calc <calc@list.ru>
2014-06-24 15:03:35 +0400

    remove duplicate

--- themes/lk/views/layouts/column2columns.php
+++ themes/lk/views/layouts/column2columns.php
@@ -4,28 +4,14 @@
 
 <table id="layout">
     <tr>
-        <td id="left_menu">
-            <!-- MENU START -->
-            <!--<div id="menu">-->
-            <div class="menu">
-                <a href="<?=$this->createUrl("cam/live")?>" class="cam">&nbsp;</a><br>
-                <a href="<?=$this->createUrl("cam/archive")?>" class="arc">&nbsp;</a><br>
-                <a href="<?=$this->createUrl("cam/balans")?>" class="bal">&nbsp;</a><br>
-                <a href="<?=$this->createUrl("cam/settings")?>" class="set">&nbsp;</a><br>
-                <a href="<?=$this->createUrl("cam/news")?>" class="news">&nbsp;</a><br>
-            </div>
-            <!-- MENU END -->
-        </td>
+        <?php $this->renderPartial('/default/menu'); ?>
         <td id="content">
             <!-- MAIN BLOCK START -->
             <div id="main">
                 <div class="content">
                     <div class="logo_small">
-                        <a href="<?=$this->createUrl('/site/logout')?>">logout</a>
                         <a href="<?=$this->createUrl("/cam")?>" class="img">
-                            &nbsp;<br>
-                            UserID:<?=MyYii::app()->user->id?><br>
-                            UserName:<?=MyYii::app()->user->name?>
+                            &nbsp;
                         </a>
                     </div>
                     <div style="height: 90px;" class="">
--- themes/lk/views/layouts/column2logo.php
+++ themes/lk/views/layouts/column2logo.php
@@ -4,18 +4,7 @@
 
 <table id="layout">
     <tr>
-        <td id="left_menu">
-            <!-- MENU START -->
-            <!--<div id="menu">-->
-            <div class="menu">
-                <a href="<?=$this->createUrl("cam/live")?>" class="cam">&nbsp;</a><br>
-                <a href="<?=$this->createUrl("cam/archive")?>" class="arc">&nbsp;</a><br>
-                <a href="<?=$this->createUrl("cam/balans")?>" class="bal">&nbsp;</a><br>
-                <a href="<?=$this->createUrl("cam/settings")?>" class="set">&nbsp;</a><br>
-                <a href="<?=$this->createUrl("cam/news")?>" class="news">&nbsp;</a><br>
-            </div>
-            <!-- MENU END -->
-        </td>
+        <?php $this->renderPartial('/default/menu'); ?>
         <td id="content">
             <!-- MAIN BLOCK START -->
             <div id="main">
